675031e0499fcbc40407fb6a5f29e97cce5bb64b
mehdi00712 <mbhoyroo246@gmail.com>
2025-09-13 16:48:09 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// Import Firebase SDK (modular)
+// firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCPHo6WngPJlbZNUnzjPLpU63aHr-OdQC8",
   authDomain: "asia-23990.firebaseapp.com",
@@ -12,13 +10,11 @@ const firebaseConfig = {
   storageBucket: "asia-23990.firebasestorage.app",
   messagingSenderId: "175190666308",
   appId: "1:175190666308:web:df9b594af164516d7e2011",
-  measurementId: "G-WJSXTQPD0Z"
+  measurementId: "G-WJSXTQPD0Z",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { app, auth, db, storage };
+export { app, auth, db };

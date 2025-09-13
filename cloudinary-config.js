2a832623130c19871a46a2927afb7dacdd140b63
mehdi00712 <mbhoyroo246@gmail.com>
2025-09-13 16:48:56 +0400

    Create cloudinary-config.js

---
+++ cloudinary-config.js
@@ -0,0 +1,9 @@
+// cloudinary-config.js
+export const CLOUDINARY = {
+  cloudName: "awasia",
+  uploadPreset: "dlwk13ady", // unsigned preset
+  folder: "products",        // optional: groups assets in Cloudinary
+};
+
+export const CLOUDINARY_UPLOAD_URL =
+  `https://api.cloudinary.com/v1_1/${CLOUDINARY.cloudName}/image/upload`;
